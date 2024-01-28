740b4f8bc403b554faee859714a37908341d16c9
Ruben Molina <rmolina@gmail.com>
2024-01-28 07:46:31 -0500

    Fix indent

--- src/classreach13.f90
+++ src/classreach13.f90
@@ -6,24 +6,24 @@
 
 !reach data structure
 MODULE Class_Reach
-	USE nrtype
+    USE nrtype
 
-	IMPLICIT NONE
+    IMPLICIT NONE
 
-	TYPE Reach_type
-		CHARACTER(LEN=30) :: rlab='', rname =''
-		REAL(DP) :: xrdn=0.0_DP
+    TYPE Reach_type
+        CHARACTER(LEN=30) :: rlab='', rname =''
+        REAL(DP) :: xrdn=0.0_DP
 !		REAL(DP) xup,xdown, xl, xreach			!xl - reach lengh (m), xreach - reach length (km)
 !		REAL(DP) elemLen							!element length
 !		INTEGER(I4B) elems						!number of elements in this reach
-!		REAL(DP) elev1, elev2					
+!		REAL(DP) elev1, elev2
 !		REAL(DP) latd, latm, lats
 !		REAL(DP) lond, lonm, lons
 !		REAL(DP) latr, lonr						!latitude, longitude radius
-		!xpm - center of reach,				!
-		REAL(DP) :: xpm =0
-		INTEGER(I4B) :: upID=0, dwnID=0			!upstream and downstream element ID
-	END TYPE Reach_type
+        !xpm - center of reach,				!
+        REAL(DP) :: xpm =0
+        INTEGER(I4B) :: upID=0, dwnID=0			!upstream and downstream element ID
+    END TYPE Reach_type
 
 END MODULE Class_Reach
 
