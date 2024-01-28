85e50b07dcc11642a5b04b57e9ba1e7d8c1c2bcf
Ruben Molina <rmolina@gmail.com>
2024-01-28 07:52:16 -0500

    Fix indent

--- src/classwaterquality.f90
+++ src/classwaterquality.f90
@@ -1,13 +1,13 @@
 !classwaterquality.f90
 !water quality constituent type
 MODULE Class_WaterQuality
-	USE nrtype
-  IMPLICIT NONE
+    USE nrtype
+    IMPLICIT NONE
 
-	TYPE WaterQuality_type
-		REAL(DP) :: Te =0.0
-		REAL(DP) :: c(1:nv) =0.0
-		REAL(DP) :: pH = 7.0
-	END TYPE
+    TYPE WaterQuality_type
+        REAL(DP) :: Te =0.0
+        REAL(DP) :: c(1:nv) =0.0
+        REAL(DP) :: pH = 7.0
+    END TYPE
 
 END MODULE
